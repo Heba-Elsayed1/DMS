271a15da6afbf6fd7c6d2dd9bb22ad190ca56b42
Heba Elsayed <heba.elsayed.m.116@gmail.com>
2025-05-01 00:11:52 +0300

    Add Backend

---
+++ backend/Application/Interface/IWorkspaceService.cs
@@ -0,0 +1,21 @@
+﻿using Application.Common;
+using Application.Dto;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interface
+{
+    public interface IWorkspaceService
+    {
+        Task<GenericResult<IEnumerable<WorkspaceDto>>> GetAllWorkspaces();
+        Task<GenericResult<WorkspaceDto>> GetWorkspaceById(int id, int userId);
+        Task<GenericResult<WorkspaceDto>> GetWorkspaceByUser(int userId);
+        Task<Result> UpdateWorkspace (WorkspaceDto workspaceDto, int userId);
+        Task<bool> WorkspaceExists (string name);
+
+    }
+}
